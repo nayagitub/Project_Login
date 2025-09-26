dc02b6e2f44d709520981b8c7b2ca8f41c8cf4e2
nayagitub <nayy8618@gmail.com>
2025-09-26 10:31:44 +0700

    mendesain From Login

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+
+    <h2>Form Login</h2>
+    <form action="" method="post">
+        Username : <input type="text" name="username" required><br><br>
+        Password : <input type="password" name="paswword" required><br><br>
+        <button type="submit">Login</button>
+    </form>
+    
+</body>
+</html>
